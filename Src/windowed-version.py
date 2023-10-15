e08948b00c48ac4377aea378b8c538c1ca053462
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-10-15 13:53:57 +0100

    Increased UI Size, Removed Exit, Added Icon, Fixed Typo, Changed Font

--- Src/windowed-version.py
+++ Src/windowed-version.py
@@ -1,16 +1,21 @@
 # Import necessary libraries
 import sys
+
 import tkinter as tk
 from tkinter import ttk, messagebox
+
 from ttkthemes import ThemedStyle
+
+from PIL import Image, ImageTk
 
 # Prevent the generation of .pyc (Python bytecode) files
 sys.dont_write_bytecode = True
 
 # Import necessary functions from the main module
 from main import generate_filename, generate_image, get_font_paths
-# Import necessary functions from the constants module
-from constants import VALID_COLORS_BY_FONT, CLOSING_MESSAGE
+
+# Import necessary constants from the constants module
+from constants import VALID_COLORS_BY_FONT
 
 # Function to generate and display an image based on user input
 def generate_and_display_image():
@@ -19,10 +24,6 @@ def generate_and_display_image():
     color = color_var.get()
 
     try:
-        if text.lower() == 'exit':
-            messagebox.showinfo("Info", CLOSING_MESSAGE)
-            root.quit()
-
         # Check for empty input
         if not text.strip():
             messagebox.showerror("Error", "Input text is empty. Please enter some text.")
@@ -66,6 +67,9 @@ def toggle_theme():
 # Create the main window
 root = tk.Tk()
 root.title("Metal Slug Font")
+im = Image.open('Assets/Icon/Raven.ico')
+photo = ImageTk.PhotoImage(im)
+root.wm_iconphoto(True, photo)
 
 # Apply the initial dark theme
 style = ThemedStyle(root)
@@ -76,37 +80,38 @@ frame = ttk.Frame(root, padding=20)
 frame.pack(expand=True, fill="both")
 
 # Label for text input
-text_label = ttk.Label(frame, text="Text to Generate:")
+text_label = ttk.Label(frame, text="Text to Generate:", font=("Ubuntu", 14))
 text_label.grid(row=0, column=0, columnspan=3, sticky="w")
 
 # Text input field
-text_entry = ttk.Entry(frame)
+text_entry = ttk.Entry(frame, font=("Ubuntu", 14))
 text_entry.grid(row=1, column=0, columnspan=3, sticky="ew")
 
 # Label for font selection
-font_label = ttk.Label(frame, text="Select Font:")
+font_label = ttk.Label(frame, text="Select Font:", font=("Ubuntu", 14))
 font_label.grid(row=2, column=0, columnspan=3, sticky="w")
 
 # Font selection dropdown
 font_var = tk.StringVar()
 font_var.set("1")  # Default font selection
-font_combobox = ttk.Combobox(frame, textvariable=font_var, values=["1", "2", "3", "4", "5"])
+font_combobox = ttk.Combobox(frame, textvariable=font_var, values=["1", "2", "3", "4", "5"], font=("Ubuntu", 14))
 font_combobox.grid(row=3, column=0, columnspan=3, sticky="ew")
 
 # Label for color selection
-color_label = ttk.Label(frame, text="Select Color:")
+color_label = ttk.Label(frame, text="Select Color:", font=("Ubuntu", 14))
 color_label.grid(row=4, column=0, columnspan=3, sticky="w")
 
 # Color selection dropdown
 color_var = tk.StringVar()
 color_var.set("Blue")  # Default color selection
-color_combobox = ttk.Combobox(frame, textvariable=color_var, values=[])
+color_combobox = ttk.Combobox(frame, textvariable=color_var, values=[], font=("Ubuntu", 14))
 color_combobox.grid(row=5, column=0, columnspan=3, sticky="ew")
 
 # Bind the font selection change event
-font_var.trace("w", on_font_change)
+font_var.trace_add("write", on_font_change)
 
 # Generate button
+style.configure("TButton", font=("Ubuntu", 14))
 generate_button = ttk.Button(frame, text="Generate and Save Image", command=generate_and_display_image)
 generate_button.grid(row=6, column=0, columnspan=3, sticky="ew")
 
@@ -120,7 +125,7 @@ theme_button.grid(row=8, column=0, columnspan=3, sticky="ew")
 
 # Add padding and make widgets expand
 for child in frame.winfo_children():
-    child.grid_configure(padx=5, pady=5, sticky="nsew")
+    child.grid_configure(padx=10, pady=10, sticky="nsew")
 
 # Run the tkinter main loop
 root.mainloop()
