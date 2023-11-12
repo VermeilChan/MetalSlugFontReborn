4a10c80eec684cf50d54e901aa01f53ab4ae235c
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-11-12 16:31:05 +0100

    Added Spacing Support, Removed Unused Imports, Changed Output Directory Name

--- Src/main.py
+++ Src/main.py
@@ -1,13 +1,13 @@
+import random, string, os, sys
+
 from collections import namedtuple
-from constants import SPACE_WIDTH , SPECIAL_CHARACTERS
-import random , string , os
 from typing import final
-from rich import console
-import cv2 , numpy
 
-from PIL import Image , ImageDraw
+import cv2, numpy
 
-conosle = console.Console()
+sys.dont_write_bytecode = True
+
+from constants import SPACE_WIDTH, SPECIAL_CHARACTERS
 
 @final
 class ImageGeneration(object):
@@ -21,14 +21,12 @@ class ImageGeneration(object):
     def GenerateFilename(self):
         self.character = ""
         for i in range(0 , 15):
-
             random_character = random.choice(string.ascii_letters + string.digits)        
             self.character += random_character
 
         return f"{self.character}.png"
-    
-    def GetFontPath(self):
 
+    def GetFontPath(self):
         self.base = os.path.join('static' , 'assets' , 'fonts' , f'font-{self._font}' , f'Font-{self._font}-{self._color}')
 
         self.container = namedtuple('Font' , ['Symbols' , 'Letters' , 'Numbers'])
@@ -38,26 +36,20 @@ class ImageGeneration(object):
         self.container.Numbers = os.path.join(self.base, 'Numbers')
 
         return self.container
-    
-    def GetCharacterImagePath(self , char:str):
 
+    def GetCharacterImagePath(self , char:str):
         letters_folder   = self.GetFontPath().Letters
         numbers_folder   = self.GetFontPath().Numbers
         symbol_folder    = self.GetFontPath().Symbols
 
         if char.isalpha():
-
             folder = 'Lower-Case' if char.islower() else 'Upper-Case'
 
             self.char_img_path = os.path.join('src' , letters_folder, folder, f"{char}.png")
 
-
         elif char.isdigit():
-
             self.char_img_path = os.path.join('src' , numbers_folder, f"{char}.png")
-
         else:
-
             self.char_img_path = os.path.join('src' , symbol_folder, f"{SPECIAL_CHARACTERS.get(char, '')}.png")
 
         return os.path.abspath(self.char_img_path)
@@ -65,36 +57,27 @@ class ImageGeneration(object):
     def GenerateImage(self): 
 
         try:
-            os.mkdir('src/static/out')
+            os.mkdir('src/static/Generated-Images')
         except Exception:
             pass
 
-        self.filename = os.path.join('src' , 'static' , 'out' , self.GenerateFilename())
+        self.filename = os.path.join('src' , 'static' , 'Generated-Images' , self.GenerateFilename())
         self.images = []
         for character in self._user_input:
 
-
             if character == " ":
+                space_image = numpy.zeros((self._image.shape[0], SPACE_WIDTH, 4), dtype=numpy.uint8)
+                self.images.append(space_image)
                 continue
 
             self._image = cv2.imread(self.GetCharacterImagePath(character) , cv2.IMREAD_UNCHANGED)
             self.images.append(self._image)
 
         try:
-
             self.horizentaly = cv2.hconcat(self.images)
             cv2.imwrite(self.filename , self.horizentaly)
 
         except Exception as error:
             print(error)
 
-
-
         return self.filename
-        
-       
-
-
-x = ImageGeneration('HELLO WORLD' , 1 , 'Orange-1')
-
-x.GenerateImage()
