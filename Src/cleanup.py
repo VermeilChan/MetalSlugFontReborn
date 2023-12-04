28dd21553e8591e299c9276b83258c21347e4525
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-12-04 04:32:40 +0100

    Idk

--- Src/cleanup.py
+++ Src/cleanup.py
@@ -7,13 +7,9 @@ def remove_images(directory_path):
         for file_path in directory.glob("*.png"):
             file_path.unlink()
             print(f"Removed: {file_path}")
-    else:
-        print(f"Directory not found: {directory}")
 
 wsgi_directory_path = "/home/Vermeil/MetalSlugFontReborn/Src/static/Generated-Images/"
 local_directory_path = "Src/static/Generated-Images/"
 
 remove_images(wsgi_directory_path)
-
-if not Path(wsgi_directory_path).exists():
-    remove_images(local_directory_path)
+remove_images(local_directory_path)
