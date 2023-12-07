20a7492470a0e86ba92be680fae616d048989f52
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-12-07 01:20:33 +0100

    Refactor code for improved readability and maintainability
    Changes:
    - Updated variable names.
    - Changed file/folder creation and paths for better organization.
    - Enhanced handling of character images using a dictionary.
    - Replaced 'cv2.hconcat' with 'np.hstack'.
    - Improved error handling and messages in generate_image method.

--- Src/main.py
+++ Src/main.py
@@ -22,8 +22,8 @@ def clean_url(url):
     return output_url
 
 class CharacterNotFound(Exception):
-    def __init__(self, unsupported_characters: str) -> None:
-        super().__init__(f"Unsupported Character '{unsupported_characters}', Try another font or ")
+    def __init__(self, unsupported_character: str) -> None:
+        super().__init__(f"Unsupported Character '{unsupported_character}', Try another font or ")
         self.errno = 10
 
 @final
@@ -34,7 +34,6 @@ class ImageGeneration(object):
         self._user_input = user_input
         self._font = font
         self._color = color
-        self._image = None
         self._font_paths = self._compute_font_paths()
 
     def _compute_font_paths(self):
@@ -45,33 +44,23 @@ class ImageGeneration(object):
         FontPaths.Numbers = os.path.join(base, 'Numbers')
         return FontPaths
 
-# development server
-#
-#    def _compute_font_paths(self):
-#        base = os.path.join('static', 'assets', 'fonts', f'font-{self._font}', f'Font-{self._font}-{self._color}')
-#        FontPaths = namedtuple('Font', ['Symbols', 'Letters', 'Numbers'])
-#        FontPaths.Symbols = os.path.join(base, 'Symbols')
-#        FontPaths.Letters = os.path.join(base, 'Letters')
-#        FontPaths.Numbers = os.path.join(base, 'Numbers')
-#        return FontPaths
-
-    def _get_character_image_path(self, char: str):
+    def _get_character_image_path(self, character: str):
         try:
             letters_folder = self._font_paths.Letters
             numbers_folder = self._font_paths.Numbers
             symbol_folder = self._font_paths.Symbols
 
-            if char.isalpha():
-                folder = 'Lower-Case' if char.islower() else 'Upper-Case'
-                char_img_path = os.path.join('src', letters_folder, folder, f"{char}.png")
-            elif char.isdigit():
-                char_img_path = os.path.join('src', numbers_folder, f"{char}.png")
+            if character.isalpha():
+                folder = 'Lower-Case' if character.islower() else 'Upper-Case'
+                character_img_path = os.path.join('src', letters_folder, folder, f"{character}.png")
+            elif character.isdigit():
+                character_img_path = os.path.join('src', numbers_folder, f"{character}.png")
             else:
-                char_img_path = os.path.join('src', symbol_folder, f"{SPECIAL_CHARACTERS.get(char, '')}.png")
-                if not os.path.exists(char_img_path):
-                    raise CharacterNotFound(char)
+                character_img_path = os.path.join('src', symbol_folder, f"{SPECIAL_CHARACTERS.get(character, '')}.png")
+                if not os.path.exists(character_img_path):
+                    raise CharacterNotFound(character)
 
-            return os.path.abspath(char_img_path)
+            return os.path.abspath(character_img_path)
 
         except CharacterNotFound as error:
             raise error
@@ -79,44 +68,34 @@ class ImageGeneration(object):
             return str(error)
 
     def generate_filename(self):
-        random_characters = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(15))
-        return f"{random_characters}.png"
+        random_filename = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(15))
+        return f"{random_filename}.png"
 
     def generate_image(self):
         try:
-            os.makedirs('/home/Vermeil/MetalSlugFontReborn/Src/static/Generated-Images', exist_ok=True)
-        except FileExistsError:
-            pass
+            os.makedirs(os.path.join('/home/Vermeil/MetalSlugFontReborn/Src/static/Generated-Images'), exist_ok=True)
+        except OSError as e:
+            return str(e)
 
-        filename = '/home/Vermeil/MetalSlugFontReborn/Src/static/Generated-Images/' + self.generate_filename()
+        filename = os.path.join('/home/Vermeil/MetalSlugFontReborn/Src/static/Generated-Images/', self.generate_filename())
         images = []
 
-# development server
-#
-#    def generate_image(self):
-#        try:
-#            os.mkdir(os.path.join('src', 'static', 'Generated-Images'))
-#        except FileExistsError:
-#            pass
-#
-#        filename = os.path.join('src', 'static', 'Generated-Images', self.generate_filename())
-#        images = []
-
-        zero_image = np.zeros((1, 1, 4), dtype=np.uint8)
+        zero_image = np.zeros((1, SPACE_WIDTH, 4), dtype=np.uint8)
+        character_images = {}
 
         for character in self._user_input:
             try:
                 if character.isspace():
-                    space_image = np.zeros((zero_image.shape[0], SPACE_WIDTH, 4), dtype=np.uint8)
-                    images.append(space_image)
+                    images.append(np.zeros_like(zero_image))
                     continue
 
-                char_image_path = self._get_character_image_path(character)
-                char_image = cv2.imread(char_image_path, cv2.IMREAD_UNCHANGED)
-                if char_image is None:
-                    raise FileNotFoundError(f"Image not found for character '{character}'")
+                if character not in character_images:
+                    character_image_path = self._get_character_image_path(character)
+                    character_images[character] = cv2.imread(character_image_path, cv2.IMREAD_UNCHANGED)
+                    if character_images[character] is None:
+                        raise FileNotFoundError(f"Image not found for character '{character}'")
 
-                images.append(char_image)
+                images.append(character_images[character])
 
             except (cv2.error, FileNotFoundError) as error:
                 return str(error)
@@ -125,7 +104,7 @@ class ImageGeneration(object):
             max_height = max(image.shape[0] for image in images)
             resized_images = [cv2.resize(image, (image.shape[1], max_height)) for image in images]
 
-            horizontally = cv2.hconcat(resized_images)
+            horizontally = np.hstack(resized_images)
             cv2.imwrite(filename, horizontally)
         except Exception as write_error:
             return str(write_error)
