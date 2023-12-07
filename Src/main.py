bae325d0d7217c6385b7ecf14c783d76b7949b1f
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-12-07 23:49:11 +0100

    Added Caching

--- Src/main.py
+++ Src/main.py
@@ -15,6 +15,8 @@ def get_font_paths(font, color):
     base_path = Path('Assets') / 'Fonts' / f'Font-{font}' / f'Font-{font}-{color}'
     return [base_path / folder for folder in ['Letters', 'Numbers', 'Symbols']]
 
+character_image_cache = {}
+
 def get_character_image_path(character, font_paths):
     CHARACTERS_FOLDER, NUMBERS_FOLDER, SYMBOLS_FOLDER = font_paths
 
@@ -30,41 +32,46 @@ def get_character_image_path(character, font_paths):
 
     return character_image_path if character_image_path.is_file() else None
 
-def create_character_image(character, font_paths):
-    if character.isspace():
-        return Image.new("RGBA", (SPACE_WIDTH, 1), (0, 0, 0, 0))
+def get_character_image(character, font_paths):
+    if character in character_image_cache:
+        return character_image_cache[character]
 
     character_image_path = get_character_image_path(character, font_paths)
     if not character_image_path or not character_image_path.is_file():
         raise FileNotFoundError(f"Error: Image not found for character '{character}'")
 
-    return Image.open(character_image_path).convert("RGBA")
+    image = Image.open(character_image_path).convert("RGBA")
+    character_image_cache[character] = image
+
+    return image
 
 def calculate_total_width_and_max_height(text, font_paths):
     total_width = 0
     max_height = 0
 
     for character in text:
-        character_image = create_character_image(character, font_paths)
+        character_image = get_character_image(character, font_paths)
         max_height = max(max_height, character_image.height)
         total_width += character_image.width
 
     return total_width, max_height
 
-def paste_character_images_to_final_image(text, character_images, final_image):
+def paste_character_images_to_final_image(text, font_paths, total_width, max_height):
     x_position = 0
-
-    for character in text:
-        final_image.paste(character_images[character], (x_position, 0))
-        x_position += character_images[character].width
-
-def generate_image(text, filename, font_paths):
-    image_path = Path(DESKTOP_PATH) / filename
-    total_width, max_height = calculate_total_width_and_max_height(text, font_paths)
-    character_images = {character: create_character_image(character, font_paths) for character in text}
     final_image = Image.new("RGBA", (total_width, max_height), (0, 0, 0, 0))
 
-    paste_character_images_to_final_image(text, character_images, final_image)
+    for character in text:
+        character_image = get_character_image(character, font_paths)
+        final_image.paste(character_image, (x_position, 0))
+        x_position += character_image.width
+
+    return final_image
+
+def generate_image(text, filename, font_paths):
+    total_width, max_height = calculate_total_width_and_max_height(text, font_paths)
+    final_image = paste_character_images_to_final_image(text, font_paths, total_width, max_height)
+    
+    image_path = Path(DESKTOP_PATH) / filename
     final_image.save(image_path)
 
     return str(image_path), None
