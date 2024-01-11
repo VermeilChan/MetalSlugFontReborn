45bd679e95ee315b4f3410d7fc0472176bc7d898
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-01-11 21:07:07 +0100

    Removed Caching

--- Src/main.py
+++ Src/main.py
@@ -19,24 +19,11 @@ class CharacterNotFound(Exception):
         super().__init__(f"Unsupported Character '{unsupported_character}', Try another font or ")
         self.errno = 10
 
+
 @final
-class ImageGeneration(object):
-    state = False
-
-    def __init__(self, user_input: str, font: int, color: str) -> None:
-        self._user_input = user_input
-        self._font = font
-        self._color = color
-        self._font_paths = self._compute_font_paths()
-        self._character_images_cache = {}
-
-    def _compute_font_paths(self):
-        base = path.join('/home/Vermeil/MetalSlugFontReborn/Src/static/assets/fonts', f'font-{self._font}', f'Font-{self._font}-{self._color}')
-        FontPaths = namedtuple('Font', ['Symbols', 'Letters', 'Numbers'])
-        FontPaths.Letters = path.join(base, 'Letters')
-        FontPaths.Numbers = path.join(base, 'Numbers')
-        FontPaths.Symbols = path.join(base, 'Symbols')
-        return FontPaths
+class ImageGenerator(object):
+    def __init__(self, font_paths):
+        self._font_paths = font_paths
 
     def _get_character_image_path(self, character: str):
         try:
@@ -62,14 +49,67 @@ class ImageGeneration(object):
             return str(error)
 
     def _load_character_image(self, character: str):
-        if character not in self._character_images_cache:
-            character_image_path = self._get_character_image_path(character)
-            image = Image.open(str(character_image_path)).convert("RGBA")
-            if image is None:
-                raise FileNotFoundError(f"Image not found for character '{character}'")
-            self._character_images_cache[character] = image
+        character_image_path = self._get_character_image_path(character)
+        image = Image.open(str(character_image_path)).convert("RGBA")
+        if image is None:
+            raise FileNotFoundError(f"Image not found for character '{character}'")
 
-        return self._character_images_cache[character]
+        return image
+
+    def get_or_create_character_image(self, character):
+        if character.isspace():
+            return self.create_character_image(character)
+
+        character_image = self._load_character_image(character)
+        return character_image
+
+    def create_character_image(self, character):
+        if character.isspace():
+            return Image.new("RGBA", (SPACE_WIDTH, 1), (0, 0, 0, 0))
+
+    def calculate_total_width_and_max_height(self, user_input):
+        total_width = 0
+        max_height = 0
+
+        for character in user_input:
+            character_image = self.get_or_create_character_image(character)
+            max_height = max(max_height, character_image.height)
+            total_width += character_image.width
+
+        return total_width, max_height
+
+    def paste_character_images_to_final_image(self, user_input, total_width, max_height):
+        x_position = 0
+        final_image = Image.new("RGBA", (total_width, max_height), (0, 0, 0, 0))
+
+        for character in user_input:
+            character_image = self.get_or_create_character_image(character)
+
+            y_position = max_height - character_image.height
+
+            final_image.paste(character_image, (x_position, y_position))
+            x_position += character_image.width
+
+        return final_image
+
+
+class ImageGeneration(object):
+    state = False
+
+    def __init__(self, user_input: str, font: int, color: str) -> None:
+        self._user_input = user_input
+        self._font = font
+        self._color = color
+        self._font_paths = self._compute_font_paths()
+        self._image_generator = ImageGenerator(self._font_paths)
+
+    def _compute_font_paths(self):
+        base = path.join('/home/Vermeil/MetalSlugFontReborn/Src/static/assets/fonts', f'font-{self._font}', f'Font-{self._font}-{self._color}')
+        FontPaths = namedtuple('Font', ['Symbols', 'Letters', 'Numbers'])
+        FontPaths.Letters = path.join(base, 'Letters')
+        FontPaths.Numbers = path.join(base, 'Numbers')
+        FontPaths.Symbols = path.join(base, 'Symbols')
+        return FontPaths
 
     def generate_filename(self):
         random_filename = ''.join(choice(ascii_letters + digits) for _ in range(15))
@@ -82,8 +122,8 @@ class ImageGeneration(object):
             return str(e)
 
         filename = path.join('/home/Vermeil/MetalSlugFontReborn/Src/static/Generated-Images/', self.generate_filename())
-        total_width, max_height = self.calculate_total_width_and_max_height()
-        final_image = self.paste_character_images_to_final_image(total_width, max_height)
+        total_width, max_height = self._image_generator.calculate_total_width_and_max_height(self._user_input)
+        final_image = self._image_generator.paste_character_images_to_final_image(self._user_input, total_width, max_height)
 
         try:
             final_image.save(filename)
@@ -94,43 +134,3 @@ class ImageGeneration(object):
 
         self.state = True
         return relative_path
-
-    def get_or_create_character_image(self, character):
-        if character.isspace():
-            return self.create_character_image(character)
-
-        character_image_path = self._get_character_image_path(character)
-        if character_image_path is None or not character_image_path.is_file():
-            raise FileNotFoundError(f"Image not found for character '{character}'")
-
-        image = Image.open(str(character_image_path)).convert("RGBA")
-        return image
-
-    def create_character_image(self, character):
-        if character.isspace():
-            return Image.new("RGBA", (SPACE_WIDTH, 1), (0, 0, 0, 0))
-
-    def calculate_total_width_and_max_height(self):
-        total_width = 0
-        max_height = 0
-
-        for character in self._user_input:
-            character_image = self.get_or_create_character_image(character)
-            max_height = max(max_height, character_image.height)
-            total_width += character_image.width
-
-        return total_width, max_height
-
-    def paste_character_images_to_final_image(self, total_width, max_height):
-        x_position = 0
-        final_image = Image.new("RGBA", (total_width, max_height), (0, 0, 0, 0))
-
-        for character in self._user_input:
-            character_image = self.get_or_create_character_image(character)
-
-            y_position = max_height - character_image.height
-
-            final_image.paste(character_image, (x_position, y_position))
-            x_position += character_image.width
-
-        return final_image
