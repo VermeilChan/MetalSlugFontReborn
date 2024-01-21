9f4513a3b861598ae9cd97140d9657d2b89b60e3
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-01-21 23:58:10 +0100

    Added Yellow

--- Src/windowed-version.py
+++ Src/windowed-version.py
@@ -21,7 +21,7 @@ VALID_COLORS_BY_FONT = {
     1: ["Blue", "Orange-1", "Orange-2"],
     2: ["Blue", "Orange-1", "Orange-2"],
     3: ["Blue", "Orange-1"],
-    4: ["Blue", "Orange-1"],
+    4: ["Blue", "Orange-1", "Yellow"],
     5: ["Orange-1"]
 }
 
