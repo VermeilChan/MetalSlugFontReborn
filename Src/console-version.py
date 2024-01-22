6c137225622547ec757c0c946a911334d5b17214
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-01-22 08:28:35 +0100

    works ig?

--- Src/console-version.py
+++ Src/console-version.py
@@ -1,5 +1,4 @@
 import sys
-
 from main import generate_filename, generate_image, get_font_paths
 
 VALID_COLORS_BY_FONT = {
@@ -11,22 +10,20 @@ VALID_COLORS_BY_FONT = {
 }
 
 def display_intro_message():
-    print("\nNote:\nConverting your text to the Metal Slug font may not work with all fonts.\n")
+    print("Note:\nConverting your text to the Metal Slug font may not work with all fonts.\n")
 
 def get_user_input():
     return input("\nEnter the text you want to generate: ")
 
 def select_font_and_color():
     while True:
+        font_input = input("Choose a font from 1 to 5 (type 'exit' to close): ")
+        if font_input.lower() == 'exit':
+            print('\nClosing...\n')
+            sys.exit(0)
+
         try:
-            user_input = input("Choose a font from 1 to 5 (type 'exit' to close): ")
-
-            if user_input.lower() == 'exit':
-                print('\nClosing...\n')
-                sys.exit(0)
-
-            font = int(user_input)
-
+            font = int(font_input)
             if 1 <= font <= 5:
                 valid_colors = VALID_COLORS_BY_FONT.get(font, [])
                 print("\nAvailable colors: " + " | ".join(valid_colors))
@@ -35,33 +32,27 @@ def select_font_and_color():
                 if color_input.lower() == 'exit':
                     print('\nClosing...\n')
                     sys.exit(0)
-                elif color_input.title() in valid_colors:
-                    color_input = color_input.title()
-                    return font, color_input
-                else:
-                    print("\nInvalid color. Please choose a valid color.\n")
+
+                color = color_input.title()
+                if color in valid_colors:
+                    return font, color
+
+                print("\nInvalid color. Please choose a valid color.\n")
             else:
                 print("\nInvalid input. Please choose a font between 1 and 5.\n")
 
         except ValueError:
             print("\nInvalid input. Please enter a valid number.\n")
-        except KeyboardInterrupt:
-            print('\nClosing...\n')
-            sys.exit(0)
 
 def ask_to_check_supported_characters():
     check_supported = input("Do you want to check the supported characters? [Y/n]: ").lower()
 
     if check_supported == 'y':
-        with open("Documentation/SUPPORTED.txt", "r") as supported_file:
+        with open("Documentation/SUPPORTED.txt", "r", encoding="utf-8") as supported_file:
             content = supported_file.read()
-            print(content)
-            print("Note:")
-            print("Some characters may not load due to font limitations or terminal compatibility.")
-            print("You can open SUPPORTED.txt if it doesn't work properly\n")
+            print(f"{content}\nNote:\nSome characters may not load due to font limitations or terminal compatibility.\nYou can open SUPPORTED.txt if it doesn't work properly\n")
     elif check_supported == 'n':
         print("\nYou can check them later if you want in SUPPORTED.txt\n")
-        pass
     else:
         print("\nInvalid input.\n")
 
@@ -82,7 +73,7 @@ def generate_and_display_image(text, font, color):
         if error_message_generate:
             print(f"Error: {error_message_generate}")
         else:
-            print(f"\nYou can find the image on your desktop: \n{image_path}")
+            print(f"\nYou can find the image on your desktop:\n{image_path}")
 
     except Exception as e:
         print(f"Error: {e}")
@@ -99,7 +90,6 @@ def main():
             generate_and_display_image(text, font, color)
     except KeyboardInterrupt:
         print('\nClosing...\n')
-        sys.exit(0)
 
 if __name__ == "__main__":
     main()
