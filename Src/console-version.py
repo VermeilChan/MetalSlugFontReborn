637e41b3eb2b7132ad45abab14269034b4952b85
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-01-23 19:37:33 +0100

    Removed Bloat

--- Src/console-version.py
+++ Src/console-version.py
@@ -10,12 +10,12 @@ VALID_COLORS_BY_FONT = {
 }
 
 def display_intro_message():
-    print("Note:\nConverting your text to the Metal Slug font may not work with all fonts.")
+    print("Note:\nCheck the supported characters in SUPPORTED.txt.")
 
 def get_user_input():
     return input("Enter the text you want to generate: ")
 
-def select_font_and_color():
+def select_font():
     while True:
         font_input = input("\nChoose a font from 1 to 5 (type 'exit' to close): ")
         if font_input.lower() == 'exit':
@@ -24,15 +24,14 @@ def select_font_and_color():
         try:
             font = int(font_input)
             if 1 <= font <= 5:
-                valid_colors = VALID_COLORS_BY_FONT.get(font, [])
-                color = get_valid_color(valid_colors)
-                return font, color
+                return font
             else:
                 print("Invalid input. Please choose a font between 1 and 5.")
         except ValueError:
             print("Invalid input. Please enter a valid number.")
 
-def get_valid_color(valid_colors):
+def select_color(font):
+    valid_colors = VALID_COLORS_BY_FONT.get(font, [])
     while True:
         print("Available colors: " + " | ".join(valid_colors))
         color_input = input("Choose a color: ")
@@ -45,21 +44,6 @@ def get_valid_color(valid_colors):
             return color
 
         print("\nInvalid color. Please choose a valid color.\n")
-
-def ask_to_check_supported_characters():
-    check_supported = input("Do you want to check the supported characters? [Y/n]: ").lower()
-
-    if check_supported == 'y':
-        print_supported_characters()
-    elif check_supported != 'n':
-        print("Invalid input. Defaulting to 'n'.")
-
-    print("You can check them later if you want in SUPPORTED.txt")
-
-def print_supported_characters():
-    with open("Documentation/SUPPORTED.txt", "r", encoding="utf-8") as supported_file:
-        content = supported_file.read()
-        print(f"{content}\nNote:\nSome characters may not load due to terminal compatibility.\nYou can open SUPPORTED.txt if it doesn't work properly")
 
 def generate_and_display_image(text, font, color):
     if text.lower() == 'exit':
@@ -87,9 +71,9 @@ def generate_and_display_image(text, font, color):
 
 def main():
     display_intro_message()
-    ask_to_check_supported_characters()
 
-    font, color = select_font_and_color()
+    font = select_font()
+    color = select_color(font)
 
     try:
         while True:
