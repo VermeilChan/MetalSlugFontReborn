68d8888f9adcae71459ab6a41ee9278fe41eee5a
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-05-19 18:55:09 +0100

    Renamed `get_or_create_character_image` to `get_character_image` and changed space character height to 0

--- Src/image_generation.py
+++ Src/image_generation.py
@@ -33,9 +33,9 @@ def get_character_image_path(character, font_paths):
     return character_image_path
 
 
-def get_or_create_character_image(character, font_paths):
+def get_character_image(character, font_paths):
     if character.isspace():
-        return Image.new("RGBA", (30, 1), (0, 0, 0, 0))
+        return Image.new("RGBA", (30, 0), (0, 0, 0, 0))
 
     character_image_path = get_character_image_path(character, font_paths)
     if not character_image_path or not character_image_path.is_file():
@@ -47,7 +47,7 @@ def get_or_create_character_image(character, font_paths):
 
 
 def generate_image(text, filename, font_paths, save_location):
-    font_images = {c: get_or_create_character_image(c, font_paths) for c in set(text)}
+    font_images = {c: get_character_image(c, font_paths) for c in set(text)}
 
     total_width = sum(font_images[c].width for c in text)
     max_height = max(font_images[c].height for c in text)
