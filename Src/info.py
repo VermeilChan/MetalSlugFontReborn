04e9dd940fbc50327f487850928bb3917fc27d81
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-05-31 01:22:09 +0100

    Update dependencies and add dynamic versioning for dev env
    - Updated msfr_version to "1.9.2"
    - Updated pyinstaller_version to "6.7.0"
    - Updated pyside6_version to "6.7.1"
    - Added config_file variable with "config.ini"
    - Introduced dynamic versioning based on latest Git commit SHA
    - Added conditional datetime formatting for development builds
    - Removed commented-out datetime print code

--- Src/info.py
+++ Src/info.py
@@ -1,12 +1,26 @@
-msfr_version = "1.9.1 (Dev)"
-pyinstaller_version = "6.6.0"
-pyside6_version = "6.7.0"
+import subprocess
+from datetime import datetime
+
+msfr_version = "1.9.2 ()"
+pyinstaller_version = "6.7.0"
+pyside6_version = "6.7.1"
 pillow_version = "10.3.0"
 build_date = "N/A"
+config_file = "config.ini"
 
-# from datetime import datetime
+is_dev = True
 
-# current_datetime = datetime.now()
-# formatted_date = current_datetime.strftime("%Y-%m-%d (%A, %B %d, %Y)")
-# formatted_time = current_datetime.strftime("%H:%M:%S")
-# print(formatted_date + " (" + formatted_time + ")")
+if is_dev:
+    current_datetime = datetime.now()
+    formatted_date_time = current_datetime.strftime("%Y-%m-%d (%A, %B %d, %Y)")
+
+    try:
+        latest_commit_sha = subprocess.check_output(
+            ["git", "rev-list", "HEAD", "-1"], text=True
+        ).strip()
+        short_commit_sha = latest_commit_sha[:7]
+        msfr_version += f" ({short_commit_sha})"
+        build_date = formatted_date_time
+    except Exception:
+        msfr_version += "(N/A)"
+        build_date = formatted_date_time
