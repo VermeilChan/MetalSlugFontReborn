f8eab01e75bda863724e47d0983e3422d1645421
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-08-13 11:51:26 +0100

    ♻️ Refactor: compression prompt logic using return bool

--- Src/console-version.py
+++ Src/console-version.py
@@ -84,10 +84,7 @@ def select_save_location():
 def ask_compression():
     compress_completer = WordCompleter(["yes", "no", "y", "n"])
     compress_input = prompt("Do you want to compress the image? (Y/n): ", completer=compress_completer).lower()
-    if compress_input in {"yes", "y", ""}:
-        return True
-    else:
-        return False
+    return bool(compress_input in {"yes", "y"})
 
 
 def generate_and_info(text, font, color, save_location, compress=False):
