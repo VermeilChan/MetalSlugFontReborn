87167ec40b7d343324937c506f1910c4f721b36d
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-09-01 04:45:10 +0100

    Revert

--- Src/info.py
+++ Src/info.py
@@ -1,5 +1,25 @@
-msfr_version = "1.10.0 (6ab9e02)"
+from datetime import datetime
+from subprocess import run, DEVNULL, PIPE, CalledProcessError
+
+msfr_version = "1.10.0"
 pyinstaller_version = "6.10.0"
 pyside6_version = "6.7.2"
 pillow_version = "10.4.0"
-build_date = "2024-09-01 (Sunday, September 01, 2024)"
+build_date = "N/A"
+
+current_datetime = datetime.now()
+formatted_date_time = current_datetime.strftime("%Y-%m-%d (%A, %B %d, %Y)")
+build_date = formatted_date_time
+
+try:
+    result = run(
+        ["git", "rev-parse", "--short", "HEAD"],
+        stdout=PIPE,
+        stderr=DEVNULL,
+        text=True,
+        check=True,
+    )
+    latest_commit_sha = result.stdout.strip()
+    msfr_version += f" ({latest_commit_sha})"
+except (CalledProcessError, FileNotFoundError):
+    msfr_version += " (N/A)"
