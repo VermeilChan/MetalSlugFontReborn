434b2cea5100d6dc8469bada28bb04d82f0c0eb5
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-10-16 16:43:10 +0100

    fix: Update MSFR versioning method
    - Changed MSFR version from hardcoded string with git commit SHA to UUID for unique versioning.
    - Updated MSFR version to "1.10.1".

--- Src/info.py
+++ Src/info.py
@@ -1,26 +1,14 @@
+from uuid import uuid4
 from datetime import datetime
-from subprocess import run, DEVNULL, PIPE, CalledProcessError
 
-msfr_version = "1.9.3"
+msfr_version = "1.10.1"
 pyinstaller_version = "6.10.0"
 pyside2_version = "5.15.2.1"
 pillow_version = "10.4.0"
-build_date = "N/A"
-config_file = "config.ini"
+
+latest_commit_sha = uuid4().hex[:7]
+msfr_version += f" ({latest_commit_sha})"
 
 current_datetime = datetime.now()
 formatted_date_time = current_datetime.strftime("%Y-%m-%d (%A, %B %d, %Y)")
 build_date = formatted_date_time
-
-try:
-    result = run(
-        ["git", "rev-parse", "--short", "HEAD"],
-        stdout=PIPE,
-        stderr=DEVNULL,
-        text=True,
-        check=True,
-    )
-    latest_commit_sha = result.stdout.strip()
-    msfr_version += f" ({latest_commit_sha})"
-except (CalledProcessError, FileNotFoundError):
-    msfr_version += " (N/A)"
