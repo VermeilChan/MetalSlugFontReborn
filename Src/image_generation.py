369dc1842aeeaea99dc6a1cffae3e8d57753ce66
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-10-16 16:42:31 +0100

    feat: Add line break support and image compression
    - Introduced `compress_image` function to optimize saved images.
    - Added `apply_line_breaks` function to manage text layout by breaking lines based on the maximum words per line.
    - Updated `generate_image` function to support line breaks and adjusted the image generation logic accordingly.

--- Src/image_generation.py
+++ Src/image_generation.py
@@ -41,22 +41,49 @@ def get_character_image(character, font_paths):
     return Image.open(character_image_path)
 
 
-def generate_image(text, filename, font_paths, save_location):
-    font_images = {
-        character: get_character_image(character, font_paths) for character in set(text)
-    }
+def compress_image(image_path):
+    with Image.open(image_path) as image:
+        image.save(image_path, optimize=True)
 
-    total_width = sum(font_images[character].width for character in text)
-    max_height = max(font_images[character].height for character in text)
 
-    final_image = Image.new("RGBA", (total_width, max_height), (0, 0, 0, 0))
+def apply_line_breaks(text, max_words_per_line):
+    words = text.split()
+    return [
+        " ".join(words[i : i + max_words_per_line])
+        for i in range(0, len(words), max_words_per_line)
+    ]
 
-    x_position = 0
-    for character in text:
-        character_image = font_images[character]
-        y_position = max_height - character_image.height
-        final_image.paste(character_image, (x_position, y_position))
-        x_position += character_image.width
+
+def generate_image(text, filename, font_paths, save_location, max_words_per_line=None):
+    lines = (apply_line_breaks(text, max_words_per_line) if max_words_per_line else [text])
+
+    line_images = []
+    max_width = total_height = 0
+
+    for line in lines:
+        font_images = {character: get_character_image(character, font_paths) for character in set(line)}
+        line_width = sum(font_images[character].width for character in line)
+        line_height = max(font_images[character].height for character in line)
+
+        line_image = Image.new("RGBA", (line_width, line_height), (0, 0, 0, 0))
+        x_position = 0
+
+        for character in line:
+            character_image = font_images[character]
+            y_position = line_height - character_image.height
+            line_image.paste(character_image, (x_position, y_position))
+            x_position += character_image.width
+
+        line_images.append(line_image)
+        max_width = max(max_width, line_width)
+        total_height += line_height
+
+    final_image = Image.new("RGBA", (max_width, total_height), (0, 0, 0, 0))
+    y_position = 0
+
+    for line_image in line_images:
+        final_image.paste(line_image, (0, y_position))
+        y_position += line_image.height
 
     image_path = Path(save_location) / filename
     final_image.save(image_path)
