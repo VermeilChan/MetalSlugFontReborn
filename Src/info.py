eae607fe00322c6d0d656d827b355d9219d36436
Misato Katsuragi <120065660+VermeilChan@users.noreply.github.com.>
2024-12-12 16:37:54 +0100

    Update libraries

--- Src/info.py
+++ Src/info.py
@@ -2,11 +2,11 @@ from uuid import uuid4
 from datetime import datetime
 
 msfr_version = "1.10.1"
-pyinstaller_version = "6.10.0"
-pyside6_version = "6.7.3"
-pillow_version = "10.4.0"
+pyinstaller_version = "6.11.1"
+pyside6_version = "6.8.1"
+pillow_version = "11.0.0"
 
-latest_commit_sha = uuid4().hex[:7]  # ik im autistic
+latest_commit_sha = uuid4().hex[:7] # placeholder for sha
 msfr_version += f" ({latest_commit_sha})"
 
 current_datetime = datetime.now()
