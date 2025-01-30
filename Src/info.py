bbe9929cbe312dea681fd0df7057e039f1c3f9e3
Misato Katsuragi <120065660+VermeilChan@users.noreply.github.com.>
2025-01-30 20:39:26 +0100

    refactor(versioning): simplify version and build date logic
    - Removed variables `pyinstaller_version`, `pyside6_version`, and `pillow_version`.
    - Simplified `msfr_version` and `build_date` assignments by directly formatting strings.
    - Removed redundant `latest_commit_sha` placeholder and integrated it directly into `msfr_version`.
    - Updated `msfr_version` to "1.11.0".

--- Src/info.py
+++ Src/info.py
@@ -1,14 +1,5 @@
 from uuid import uuid4
 from datetime import datetime
 
-msfr_version = "1.10.1"
-pyinstaller_version = "6.11.1"
-pyside6_version = "6.8.1"
-pillow_version = "11.0.0"
-
-latest_commit_sha = uuid4().hex[:7] # placeholder for sha
-msfr_version += f" ({latest_commit_sha})"
-
-current_datetime = datetime.now()
-formatted_date_time = current_datetime.strftime("%Y-%m-%d (%A, %B %d, %Y)")
-build_date = formatted_date_time
+msfr_version = f"1.11.0 ({uuid4().hex[:7]})"
+build_date = datetime.now().strftime("%Y-%m-%d (%A, %B %d, %Y)")
